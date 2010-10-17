43991103ed740a5e9fa12bbd6c8f4a117e85a10b
ignatov <sergey@ignatov.spb.su>
2010-10-17 21:47:37 +0400

    Cleanup

--- src/main/scala/ExpressionDiagram.scala
+++ src/main/scala/ExpressionDiagram.scala
@@ -2,10 +2,10 @@ package net.t32leaves.syntaxDiagramGenerator
 
 import java.io.FileOutputStream
 import java.awt.{Font, Graphics2D, Dimension}
-import java.awt.geom._;
+import java.awt.geom._
 
-import org.apache.batik.svggen.SVGGraphics2D;
-import org.apache.batik.dom.svg.SVGDOMImplementation;
+import org.apache.batik.svggen.SVGGraphics2D
+import org.apache.batik.dom.svg.SVGDOMImplementation
 
 import org.apache.batik.transcoder.{TranscoderInput, TranscoderOutput}
 import org.apache.batik.transcoder.image.PNGTranscoder
@@ -14,34 +14,35 @@ import org.apache.fop.svg.PDFTranscoder
 object ExpressionDiagram {
   private val PADDING = 10
   private val PADDING_HALF = 5
-  
+
   private abstract class Graph {
     def apply
-    
+
     def w: Int
+
     def h: Int
   }
 
   private class OrGraph(children: List[Graph], g: Graphics2D) extends Graph {
     def apply: Unit = {
       val prev = g.getTransform
-      
+
       val x = w
       children.foreach(c => {
         val dw = (x - c.w) / 2
         val ch = c.h
         val dh = ch / 2
-        
+
         val path = new GeneralPath()
-        if(children.head == c) {
+        if (children.head == c) {
           path.moveTo(dw, dh + PADDING)
           path.lineTo(PADDING, dh + PADDING)
-          path.quadTo(0, dh + PADDING, 0, dh + 2*PADDING)
+          path.quadTo(0, dh + PADDING, 0, dh + 2 * PADDING)
           path.lineTo(0, ch + PADDING)
-          
+
           path.moveTo(x - dw, dh + PADDING)
           path.lineTo(x - PADDING, dh + PADDING)
-          path.quadTo(x, dh + PADDING, x, dh + 2*PADDING)
+          path.quadTo(x, dh + PADDING, x, dh + 2 * PADDING)
           path.lineTo(x, ch + PADDING)
         } else {
           path.moveTo(0, 0)
@@ -52,41 +53,41 @@ object ExpressionDiagram {
           path.lineTo(x - PADDING, PADDING + dh)
           path.quadTo(x, PADDING + dh, x, dh)
           path.lineTo(x, 0)
-          
-          if(children.last != c) {
-        	path.moveTo(0, dh)
-          	path.lineTo(0, ch + PADDING)
-          	path.moveTo(x, dh)
-          	path.lineTo(x, ch + PADDING)
+
+          if (children.last != c) {
+            path.moveTo(0, dh)
+            path.lineTo(0, ch + PADDING)
+            path.moveTo(x, dh)
+            path.lineTo(x, ch + PADDING)
           }
         }
-        
 
         g.draw(path)
-        
+
         g.translate(dw, PADDING)
         c.apply
         g.translate(-dw, ch)
       })
-      
+
       g.setTransform(prev)
       //val y = h - children.last.h
       //g.drawLine(0, 0, 0, y)
       //g.drawLine(x, 0, x, y)
     }
-    
-    def w = (0/:children)((m, e) => if(m < e.w) e.w else m) + (3 * PADDING)
-    def h = (children.length * PADDING) + (0/:children)((m, e) => m + e.h)
+
+    def w = (0 /: children)((m, e) => if (m < e.w) e.w else m) + (3 * PADDING)
+
+    def h = (children.length * PADDING) + (0 /: children)((m, e) => m + e.h)
   }
 
   private class SeqGraph(children: List[Graph], g: Graphics2D) extends Graph {
     def apply: Unit = {
       val prev = g.getTransform
-      
+
       val y = h / 2
       children.foreach(c => {
         val dh = y - (c.h / 2)
-        
+
         g.drawLine(0, y, PADDING, y)
         g.translate(PADDING, dh)
         c.apply
@@ -96,22 +97,23 @@ object ExpressionDiagram {
 
       g.setTransform(prev)
     }
-    
-    def w = (children.length * PADDING) + (0/:children)((m, e) => m + e.w) + PADDING
-    def h = (0/:children)((m, e) => if(m < e.h) e.h else m)
+
+    def w = (children.length * PADDING) + (0 /: children)((m, e) => m + e.w) + PADDING
+
+    def h = (0 /: children)((m, e) => if (m < e.h) e.h else m)
   }
 
   private abstract class CrossingGraph(child: Graph, g: Graphics2D) extends Graph {
     protected def drawDirection(offsetX: Int, offsetY: Int)
-    
+
     def apply {
       val ch = child.h
       val cw = child.w
-      
+
       g.translate(PADDING, 0)
       child.apply
       g.translate(-PADDING, 0)
-      
+
       val path = new GeneralPath
       path.moveTo(0, 0)
       path.moveTo(0, h / 2)
@@ -120,7 +122,7 @@ object ExpressionDiagram {
       path.moveTo(cw + PADDING, ch / 2)
       path.lineTo(cw + 2 * PADDING, ch / 2)
       path.quadTo(cw + 3 * PADDING, ch / 2, cw + 3 * PADDING, h / 2)
-      
+
       path.moveTo(0, h / 2)
       path.lineTo(0, ch)
       path.quadTo(0, ch + PADDING, PADDING, ch + PADDING)
@@ -128,11 +130,12 @@ object ExpressionDiagram {
       path.quadTo(cw + 3 * PADDING, ch + PADDING, cw + 3 * PADDING, ch)
       path.lineTo(cw + 3 * PADDING, h / 2)
       g.draw(path)
-      
+
       drawDirection((cw + PADDING) / 2, ch + PADDING_HALF)
     }
-    
+
     def w = child.w + (3 * PADDING)
+
     def h = child.h + (2 * PADDING)
   }
 
@@ -157,34 +160,36 @@ object ExpressionDiagram {
       g.fill(path)
     }
   }
-  
+
   private class LiteralGraph(literal: String, g: Graphics2D) extends Graph {
     def apply {
       g.drawRect(0, 0, w, h)
       g.drawLine(PADDING / 2, 0, PADDING / 2, h)
       g.drawString(literal, PADDING, h - PADDING_HALF)
     }
-    
+
     def w = g.getFontMetrics.stringWidth(literal) + (2 * PADDING)
+
     def h = g.getFontMetrics.getHeight + PADDING
   }
-  
+
   private class RuleRefGraph(ruleName: String, g: Graphics2D) extends Graph {
     def apply {
       g.drawRect(0, 0, w, h)
       g.drawString(ruleName, PADDING_HALF, h - PADDING_HALF)
     }
-    
+
     def w = g.getFontMetrics.stringWidth(ruleName) + PADDING
+
     def h = g.getFontMetrics.getHeight + PADDING
   }
-  
+
 
   def apply(expr: Expression, name: String) = {
-	  val impl = SVGDOMImplementation.getDOMImplementation();
-    val doc = impl.createDocument(SVGDOMImplementation.SVG_NAMESPACE_URI, "svg", null);
+    val impl = SVGDOMImplementation.getDOMImplementation()
+    val doc = impl.createDocument(SVGDOMImplementation.SVG_NAMESPACE_URI, "svg", null)
 
-    val g = new SVGGraphics2D(doc);
+    val g = new SVGGraphics2D(doc)
     g.setFont(new Font("Helvetica", Font.BOLD, 10))
     val titleFontHeight = g.asInstanceOf[Graphics2D].getFontMetrics.getHeight
     g.drawString(name, PADDING: Float, PADDING + titleFontHeight: Float)
@@ -192,11 +197,11 @@ object ExpressionDiagram {
     g.setFont(new Font("Courier", Font.PLAIN, 10))
     val subgr = subgraph(expr, g)
     subgr.apply
-    
-	  g.setSVGCanvasSize(new Dimension(subgr.w + 2*PADDING, subgr.h + 3*PADDING));
+
+    g.setSVGCanvasSize(new Dimension(subgr.w + 2 * PADDING, subgr.h + 3 * PADDING))
     g
   }
-  
+
   private def subgraph(expr: Expression, g: Graphics2D): Graph = expr match {
     case OrExpression(c) => new OrGraph(c.map(subgraph(_, g)), g)
     case SeqExpression(c) => new SeqGraph(c.map(subgraph(_, g)), g)
@@ -216,13 +221,13 @@ object ExpressionDiagram {
 
     // Create the transcoder output.
     val ostream = new FileOutputStream(name + "." + format)
-    val output = new TranscoderOutput(ostream);
+    val output = new TranscoderOutput(ostream)
 
     // Save the image.
-    t.transcode(input, output);
+    t.transcode(input, output)
 
     // Flush and close the stream.
-    ostream.flush();
-    ostream.close();
+    ostream.flush
+    ostream.close
   }
 }
